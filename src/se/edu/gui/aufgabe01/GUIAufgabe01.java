9145f0608e4f3b546d17ee1c7f1b919ea5f5ab93
Stefan Ebert <stefan+studium-gui@ebert-linux.de>
2011-10-15 12:56:53 +0200

    init

---
+++ src/se/edu/gui/aufgabe01/GUIAufgabe01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.edu.gui.aufgabe01;
+
+/**
+ *
+ * @author steeb
+ */
+public class GUIAufgabe01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
