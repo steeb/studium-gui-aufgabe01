9655c0b8d5753f7f6889d999982239f6f5824dc8
Stefan Ebert <stefan+studium-gui@ebert-linux.de>
2011-10-19 17:33:36 +0200

    sinnvollere Variabelnnamen vergeben

--- src/se/edu/gui/aufgabe01/diagramm/WeinBalkenDiagramm.java
+++ src/se/edu/gui/aufgabe01/diagramm/WeinBalkenDiagramm.java
@@ -10,7 +10,6 @@ import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Paint;
 import java.awt.Rectangle;
 import javax.swing.JPanel;
 
@@ -20,8 +19,13 @@ import javax.swing.JPanel;
  */
 public class WeinBalkenDiagramm extends JPanel{
     
+    int jahrgang;
+    int lagerdauer;
+    
     public WeinBalkenDiagramm(int jahrgang, int lagerdauer) {
         super();
+        this.jahrgang = jahrgang;
+        this.lagerdauer = lagerdauer;
         this.setLayout(new BorderLayout());
         this.setBackground(Color.white);
     }
@@ -29,48 +33,48 @@ public class WeinBalkenDiagramm extends JPanel{
     @Override
     protected void paintComponent(Graphics grphcs) {
         super.paintComponent(grphcs);
-        double breite;
-        int breite0, breite1, breite2, breite3;
-        int hoehe;
+        double fensterBreite;
+        int balkenBreite0, balkenBreite1, balkenBreite2, balkenBreite3;
+        int balkenHoehe;
         Graphics2D grphcs2d = (Graphics2D)grphcs;
         grphcs2d.setStroke(new BasicStroke(1));
         
-        hoehe = this.getHeight() * 25 / 100 - 10;
+        balkenHoehe = this.getHeight() * 25 / 100 - 10;
         
-        breite = this.getWidth() * 80 / 100;
-        breite0 = (int)(breite * 10 / 100);
-        breite1 = (int)(breite * 20 / 100);
-        breite2 = (int)(breite * 50 / 100);
-        breite3 = (int)(breite * 20 / 100);
+        fensterBreite = this.getWidth() * 80 / 100;
+        balkenBreite0 = (int)(fensterBreite * 10 / 100);
+        balkenBreite1 = (int)(fensterBreite * 20 / 100);
+        balkenBreite2 = (int)(fensterBreite * 50 / 100);
+        balkenBreite3 = (int)(fensterBreite * 20 / 100);
         
         //zu früh
         grphcs2d.translate(this.getWidth() / 10, this.getHeight() / 10);
         grphcs2d.setPaint(Color.GRAY);
-        grphcs2d.fill(new Rectangle(breite0, hoehe));
+        grphcs2d.fill(new Rectangle(balkenBreite0, balkenHoehe));
         grphcs2d.setPaint(Color.BLACK);
-        grphcs2d.draw(new Rectangle(breite0, hoehe));
+        grphcs2d.draw(new Rectangle(balkenBreite0, balkenHoehe));
         
         //steigert sich noch
-        grphcs2d.translate(breite0, 0);
+        grphcs2d.translate(balkenBreite0, 0);
         grphcs2d.setPaint(new GradientPaint(0, 50, Color.GRAY, 
                                             200, 50, Color.GREEN));
-        grphcs2d.fill(new Rectangle(breite1, hoehe));
+        grphcs2d.fill(new Rectangle(balkenBreite1, balkenHoehe));
         grphcs2d.setPaint(Color.BLACK);
-        grphcs2d.draw(new Rectangle(breite1, hoehe));
+        grphcs2d.draw(new Rectangle(balkenBreite1, balkenHoehe));
         
         //optimater trinkzeitpunkt
-        grphcs2d.translate(breite1, 0);
+        grphcs2d.translate(balkenBreite1, 0);
         grphcs2d.setPaint(Color.GREEN);
-        grphcs2d.fill(new Rectangle(breite2, hoehe));
+        grphcs2d.fill(new Rectangle(balkenBreite2, balkenHoehe));
         grphcs2d.setPaint(Color.BLACK);
-        grphcs2d.draw(new Rectangle(breite2, hoehe));
+        grphcs2d.draw(new Rectangle(balkenBreite2, balkenHoehe));
         
         //überlagert
-        grphcs2d.translate(breite2, 0);
+        grphcs2d.translate(balkenBreite2, 0);
         grphcs2d.setPaint(Color.YELLOW);
-        grphcs2d.fill(new Rectangle(breite3, hoehe));
+        grphcs2d.fill(new Rectangle(balkenBreite3, balkenHoehe));
         grphcs2d.setPaint(Color.BLACK);
-        grphcs2d.draw(new Rectangle(breite3, hoehe));
+        grphcs2d.draw(new Rectangle(balkenBreite3, balkenHoehe));
     }
    
 }
